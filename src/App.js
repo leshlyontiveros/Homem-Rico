c66befe7c61a7a5f0cdd6f87185ffc526d456119
leshlyontiveros <less.onti@gmail.com>
2021-11-26 18:55:55 -0300

    Subindo meu React 'Homem-Rico'

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+        <img src={"https://raw.githubusercontent.com/rquadrosDH/imagens/main/homem-feliz-dinheiro.jpg"} alt="homem rico e feliz"/>
+        <p>
+          A felicidade de uma pessoa com dinheiro.
+        </p>
+      </header>
+    </div>
+    </>
+  )
+}
+
+export default App;
